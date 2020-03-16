dbebfc826a2484ac0a02e6f74bee72943cc18d38
Paweł <pjanic98@gmail.com>
2020-03-16 19:24:06 +0100

    Initial commit

---
+++ src/test/java/com/crewmaker/CrewmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crewmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
