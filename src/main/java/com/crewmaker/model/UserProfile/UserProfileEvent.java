e6c09102e2fce8adf27ae8daaf566c18a3995479
Paweł <pjanic98@gmail.com>
2020-04-22 12:25:34 +0200

    Add UserProfile backend, UsersEvents to fix

--- src/main/java/com/crewmaker/model/UserProfile/UserProfileEvent.java
+++ src/main/java/com/crewmaker/model/UserProfile/UserProfileEvent.java
@@ -3,31 +3,14 @@ package com.crewmaker.model.UserProfile;
 import java.util.Date;
 
 public class UserProfileEvent {
-    private String sportsCategory;
     private Date date;
     private Date eventTime;
-    private String eventPlaceName;
-    private String eventPlaceCity;
-    private String eventPlaceStreet;
-    private String eventPlaceNumber;
-    private long activeParticipants;
     private int maxParticipants;
 
-    public UserProfileEvent(String sportsCategory, Date date, Date eventTime, String eventPlaceName, String eventPlaceCity,
-                            String eventPlaceStreet, String eventPlaceNumber, long activeParticipants, int maxParticipants) {
-        this.sportsCategory = sportsCategory;
+    public UserProfileEvent(Date date, Date eventTime, int maxParticipants) {
         this.date = date;
         this.eventTime = eventTime;
-        this.eventPlaceName = eventPlaceName;
-        this.eventPlaceCity = eventPlaceCity;
-        this.eventPlaceStreet = eventPlaceStreet;
-        this.eventPlaceNumber = eventPlaceNumber;
-        this.activeParticipants = activeParticipants;
         this.maxParticipants = maxParticipants;
-    }
-
-    public String getSportsCategory() {
-        return sportsCategory;
     }
 
     public Date getDate() {
@@ -36,26 +19,6 @@ public class UserProfileEvent {
 
     public Date getEventTime() {
         return eventTime;
-    }
-
-    public String getEventPlaceName() {
-        return eventPlaceName;
-    }
-
-    public String getEventPlaceCity() {
-        return eventPlaceCity;
-    }
-
-    public String getEventPlaceStreet() {
-        return eventPlaceStreet;
-    }
-
-    public String getEventPlaceNumber() {
-        return eventPlaceNumber;
-    }
-
-    public long getActiveParticipants() {
-        return activeParticipants;
     }
 
     public int getMaxParticipants() {
