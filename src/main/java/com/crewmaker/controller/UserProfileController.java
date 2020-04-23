18b9c959dbca8c9da4e7db47b0c1c3dffe8ae2f5
cygkam <cygkam@gmail.com>
2020-04-23 21:30:22 +0200

    fixed after merging

--- src/main/java/com/crewmaker/controller/UserProfileController.java
+++ src/main/java/com/crewmaker/controller/UserProfileController.java
@@ -1,8 +1,5 @@
 package com.crewmaker.controller;
 
-import com.crewmaker.entity.Event;
-import com.crewmaker.entity.User;
-import com.crewmaker.model.UserProfile.UserProfileEvent;
 import com.crewmaker.model.UserProfile.UserProfileUser;
 import com.crewmaker.repository.EventRepository;
 import com.crewmaker.repository.UserRepository;
@@ -10,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/userProfile")
@@ -29,14 +23,18 @@ public class UserProfileController {
         return userRepository.findByUsernameUserProfile(username);
     }
 
+    /*
     @GetMapping("/event/future/{id}")
     public List<Event> getUserFutureEvents(@PathVariable int id) {
         //return eventRepository.findFutureEventsByUsername(username);
         return eventRepository.findByEventId(1);
     }
 
+
+
     @GetMapping("/event/past/{id}")
     public List<Event> getUserPastEvents(@PathVariable int id) {
         return eventRepository.findByEventId(1);
     }
+    */
 }
