31a012bc046917361e907ec64303b96c311c43ce
Kamil <Cygan>
2020-04-23 21:30:22 +0200

    fixed after merging

--- src/main/java/com/crewmaker/repository/EventRepository.java
+++ src/main/java/com/crewmaker/repository/EventRepository.java
@@ -1,24 +1,21 @@
 package com.crewmaker.repository;
 
 import com.crewmaker.entity.Event;
-import com.crewmaker.entity.User;
-import com.crewmaker.model.UserProfile.UserProfileEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     // Max results?
-    List<Event> findByEventId(int id);
+    //List<Event> findByEventId(int id);
+    Event findByEventId(int id);
+
     List<Event> findAllBySportsCategorySportCategoryName(String SportCategoryName);
     List<Event> findBySportsCategorySportCategoryName(String SportCategoryName);
     List<Event> findAllBySportsCategorySportsCategoryId(int SportCategoryID);
-    Event findByEventId(int id);
+
     /*@Query("SELECT new com.crewmaker.model.UserProfile.UserProfileEvent(sc.sportCategoryName, e.date, e.eventTime, " +
             "ep.name, ep.city, ep.street, ep.streetNumber, " +
             "(SELECT Count(p2.id.event) FROM Participation p2 WHERE p2.id.event = e.eventId), e.maxPlayers) " +
