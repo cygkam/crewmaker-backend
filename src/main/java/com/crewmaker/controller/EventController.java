6159ce4d7690b45ca4ce52abf5b46684c5a21a41
litkowczak <litkowczak@examp>
2020-05-07 11:58:52 +0200

    updated event controller

--- src/main/java/com/crewmaker/controller/EventController.java
+++ src/main/java/com/crewmaker/controller/EventController.java
@@ -7,12 +7,14 @@ import com.crewmaker.exception.ResourceNotFoundException;
 import com.crewmaker.repository.EventRepository;
 import com.crewmaker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,10 +28,25 @@ public class EventController {
     UserRepository userRepository;
 
     //baeldung requestparams zeby parametryzowac tutaj jaki event i przeslac sobie id sportscategory a nie string.
-    @GetMapping("/api/searchevents")
+   /* @GetMapping("/api/searchevents")
     List<EventDTO> searchEvents(@RequestParam(name = "categoryid") int categoryID){
         return eventRepository.findAllBySportsCategorySportsCategoryId(categoryID)
                 .stream().limit(10).map(event -> new EventDTO(event)).collect(Collectors.toList());
+    }*/
+    @GetMapping("/api/searchevents")
+    List<EventDTO> searchEvents(@RequestParam(name = "categoryid") int categoryID,
+                                @RequestParam  @DateTimeFormat(pattern = "dd-MM-yyyy") Date eventDate){
+
+        //System.out.println(eventDate.toString());
+        /*return eventRepository.findAllBySportsCategorySportsCategoryId(categoryID)
+                .stream().limit(10).map(event -> new EventDTO(event)).collect(Collectors.toList());
+*/
+        return eventRepository.findAllByDateAfterAndAndSportsCategorySportsCategoryIdOrderByDate(
+                eventDate
+                ,categoryID)
+                .stream().limit(10).map(event ->  new EventDTO(event)).collect(Collectors.toList());
+        /*return eventRepository.findAllByDateAfterAndEventTimeAfterAndSportsCategorySportsCategoryIdOrderByDateAscEventTimeAsc(eventDate,eventTime,categoryID)
+                .stream().limit(10).map(e -> new EventDTO(e)).collect(Collectors.toList());*/
     }
 
     @GetMapping("/api/event")
