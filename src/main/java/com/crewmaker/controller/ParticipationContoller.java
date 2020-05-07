b0f9023d3395e1cc98edd6e4a24ed7693c4bd5b9
litkowczak <litkowczak@examp>
2020-05-07 19:22:23 +0200

    added join to event request

---
+++ src/main/java/com/crewmaker/controller/ParticipationContoller.java
@@ -0,0 +1,62 @@
+package com.crewmaker.controller;
+
+import com.crewmaker.entity.Event;
+import com.crewmaker.entity.Participation;
+import com.crewmaker.entity.User;
+import com.crewmaker.exception.ResourceNotFoundException;
+import com.crewmaker.repository.EventRepository;
+import com.crewmaker.repository.ParticipationRepository;
+import com.crewmaker.repository.UserRepository;
+import com.crewmaker.reqbody.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+public class ParticipationContoller {
+
+    @Autowired
+    EventRepository eventRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ParticipationRepository participationRepository;
+
+    @GetMapping("/api/joinevent")
+    public ResponseEntity<?> joinEvent(@RequestParam int eventID){
+        Event event = eventRepository.findByEventId(eventID);
+        long actualParticipants = eventRepository.countAllByEventParticipationsIdEvent(event);
+
+        if(actualParticipants >= event.getMaxPlayers()) throw new ResourceNotFoundException("Event","EventParticipants",event);
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        Participation newParticipation = new Participation(user,event,0);
+        participationRepository.save(newParticipation);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/users/{username}")
+                .buildAndExpand(newParticipation.getId()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+    }
+
+    @GetMapping("/api/existsparticipation")
+    public boolean existParticipation(@RequestParam int eventID){
+        Event event = eventRepository.findByEventId(eventID);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+
+        return  participationRepository.existsByIdEventAndIdUser(event,user);
+    }
+}
