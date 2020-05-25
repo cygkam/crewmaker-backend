738dc684fb57ca0b5c9a8da09636ebb64c9a7cef
litkowczak <litkowczak@examp>
2020-05-25 10:36:40 +0200

    added eventplace opinion requests

--- src/main/java/com/crewmaker/repository/EventPlaceOpinionRepository.java
+++ src/main/java/com/crewmaker/repository/EventPlaceOpinionRepository.java
@@ -4,6 +4,9 @@ import com.crewmaker.entity.EventPlaceOpinion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventPlaceOpinionRepository extends JpaRepository<EventPlaceOpinion,Long> {
+    List<EventPlaceOpinion> findAllByEventPlaceEventPlaceId(int id);
 }
