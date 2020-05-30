387dcbdfae963772892d19fe4b241bdc78a4a666
Maciej <libero2798@gmail.com>
2020-05-30 21:58:27 +0200

    Zmiana związana z fixem na frontendzie

--- src/main/java/com/crewmaker/reqbody/NewUserOpinionRequest.java
+++ src/main/java/com/crewmaker/reqbody/NewUserOpinionRequest.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotBlank;
 
 public class NewUserOpinionRequest {
 
-        private String userAuthor;
+        private String opinionAuthorName;
 
         private String userAbout;
 
